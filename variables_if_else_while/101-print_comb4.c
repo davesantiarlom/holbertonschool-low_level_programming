252907e202c70877baa253ee0dcb9b4b2e06446c
root <root@DESKTOP-V8M2LUO.localdomain>
2022-06-14 15:04:32 -0500

    101-print_comb4.c

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -8,21 +8,21 @@
  * Return: Always 0 (Success)
  */
 
- int main(void)
+int main(void)
 {
 	int i, f, g;
 
-	for(i = '0'; i < '9'; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for(f = i + 1; f <= '9'; f++)
+		for (f = i + 1; f <= '9'; f++)
 		{
-			for(g = f + 1; g <= '9'; g++)
+			for (g = f + 1; g <= '9'; g++)
 			{
 				if ((i != f) != g)
 				{
-				        putchar(i);
-					putchar(f);
-					putchar(g);
+				      putchar(i);
+				      putchar(f);
+				      putchar(g);
 
 					if (i == '7' && f == '8')
 						continue;
