3af4549bfbe560f4f5cd39f40bc1615dbd3be474
root <root@DESKTOP-V8M2LUO.localdomain>
2022-06-21 17:09:01 -0500

    task 3y4

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to stdout.
+ * _puts - Write a function that prints a string
  * @str: an input string
  * Return: Nothing
  */
