a20e9b2a0dc2349e49e5bc92240c99e42b374ab0
root <root@DESKTOP-V8M2LUO.localdomain>
2022-06-21 17:15:45 -0500

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character.
+ * @str: An input string
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int l = 0, i = 0;
+
+	while (str[l] != '\0')
+		l++;
+
+	l -= 1;
+
+	for (; i <= l; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
