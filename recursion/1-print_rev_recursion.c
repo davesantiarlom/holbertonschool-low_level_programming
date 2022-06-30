9794909c31c02bc8398d973072cbd430d410c916
root <root@DESKTOP-V8M2LUO.localdomain>
2022-06-30 18:14:30 -0500

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 /**
- * _print_rev_recursion - a function that prints a string in reverse,
- * followed by a new line.
- * @s: An input string to printing
- * Return: Nothing
- */
+* _print_rev_recursion - a function that prints a string in reverse
+* @s: An input string to printing
+* Return: Nothing
+*/
 
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
-	{ 
+	{
 		_print_rev_recursion(s + 1);
 		printf("%c", *s);
   	}
