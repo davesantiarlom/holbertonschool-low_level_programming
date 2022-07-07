23ea6fda1e29af311fb29c27a6856d52fc83d19f
root <root@DESKTOP-V8M2LUO.localdomain>
2022-07-07 15:31:46 -0500

    Tasks

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,37 +3,6 @@
 #include <stdlib.h>
 
 /**
-* alloc_grid - A function that returns a pointer to a
-* 2 dimensional array of integers
-* @width: An input integer at number of columns
-* @height: An input integer at number of rows
-* Return: pointer to a 2D array, NULL on failure
-*/
-
-int **alloc_grid(int width, int height)
-{
-	int **array, i = 0, j = 0;
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-		return (NULL);
-	for (; i < height; i++)
-	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-			return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
-		for (; j < width; j++)
-			array[i][j] = 0;
-	return (array);
-}
-
-/**
 * free_grid - A function that frees a 2D array of ints previously
 * created by your malloc_grid function
 * @grid: An input 2D array of integers to free
