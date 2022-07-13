dc1a66f4840982831ad572bd405864e5704eb0e8
root <root@DESKTOP-V8M2LUO.localdomain>
2022-07-13 16:48:49 -0500

    Tasks

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,7 +19,8 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i= 0;
+	int i = 0;
+
 	while (i < 5)
 	{
 		if (*ops[i].op == *s)
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,6 +13,7 @@
 int main(int argc, char **argv)
 {
 	int (*pf)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
