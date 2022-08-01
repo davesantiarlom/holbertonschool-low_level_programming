6b6529baf059163be8586681945d7291ca7f425a
root <root@Lombana.localdomain>
2022-08-01 18:02:09 -0500

    Tasks

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,17 +8,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	mask = mask << (sizeof(unsigned long int) * 8 - 1);
-
-	while (mask != 0)
-	{
-		if ((n & mask) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-
-	mask = mask >> 1;
-	}
+	_putchar((n & 1) + '0');
 }
