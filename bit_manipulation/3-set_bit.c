23d2166d70162a5e06c37dc191badfb8f127faf7
root <root@Lombana.localdomain>
2022-08-01 18:10:27 -0500

    Tasks

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * set_bit - A function that sets a bit at given index to 1
- * @n: The number to set bit in
- * @index: The index to set bit at
- * Return: 1 if it worked, or -1 on error
+ * set_bit - Function that sets a bit to 1 at a specific index
+ * @n: Pointer to decimal being passed in
+ * @index: The binary position to modify
+ * Return: 1 if success, -1 if failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int mask = 1;
 
-	max <<= index;
-	if (max == 0)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n |= max;
+
+	mask = mask << index;
+
+	*n = *n | mask;
+
 	return (1);
 }
